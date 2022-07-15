8279deb4740000d526a1b7c054529125a820d721
Luisjimenez2812 <luisjimenez2812@gmail.com>
2022-07-15 11:09:19 -0600

    Finalización del proyecto

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -6,11 +6,15 @@
 
 @foreach( $posts as $post )
 <p>
-    <strong>{{ $post['id'] }}</strong>
-    <a href="{{route('post', $post['slug'])}}">
-        {{ $post['title'] }}
-    </a>
+    <strong>{{ $post->id }}</strong>
+	<a href="{{ route('post', $post->slug) }}">
+		{{ $post->title }}
+	</a>
+	<br>
+	<span>{{ $post->user->name }}</span>
 </p>
 @endforeach
+
+{{ $posts->links() }}
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,27 @@
 <?php
 
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+/**
+ * Route::get('/', [PageController::class, 'home'])->name('home');
+ * Route::get('blog', [PageController::class, 'blog'])->name('blog');
+ * Route::get('blog/{slug}', [PageController::class, 'post'])->name('post');
+ */
 
-Route::get('blog', function () {
-    $posts = [
-        ['id' => 1, 'title' => 'PHP',     'slug' => 'php'],
-        ['id' => 2, 'title' => 'Laravel', 'slug' => 'laravel']
-    ];
-    return view('blog', ['posts' => $posts]);
-})->name('blog');
+Route::controller(PageController::class)->group(function () {     
 
-Route::get('blog/{slug}', function ($slug) {
-    $post = $slug;
-    return view('post', ['post' => $post]);
-})->name('post');
+    Route::get('/',                'home')->name('home');     
+    Route::get('blog',             'blog')->name('blog'); 
+    Route::get('blog/{post:slug}', 'post')->name('post'); 
 
+});
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::resource('posts', PostController::class)->except(['show']);
 
 require __DIR__.'/auth.php';
